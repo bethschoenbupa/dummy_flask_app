573985105f7a3637448a6a77c9b44100cfbd447a
Bethany <bethany.schoen@bupa.com>
2025-08-14 08:21:34 +0000

    app ready for deployment

--- app.py
+++ app.py
@@ -15,7 +15,16 @@ def is_birthday(birthday):
     raise Exception("Not correct date format")
 
   today = datetime.today()
-  if today.date() == birthday_datetime.date()
-    return "<p>Today is your birthday! Good for you.</p>"
-  else 
-    return "<p>Today is not your birthday.</p>"
+  if today.date() == birthday_datetime.date():
+    return """<body>
+  <p>Today is your birthday! Good for you.</p>
+  <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDJqMTRuanRocnhxazE3cW05Y2dtNGhpNnJuZ3IwaWxlYW42M2t0bSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Qvns6NmhC1MBLKGbL1/giphy.gif"/>
+</body>"""
+  else :
+    return """<body>
+  <p>Today is not your birthday.</p>
+  <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExanJkMmUyeDN0ZnI4bDNzZDM0NTk4N3J5M3hvNDk3d2RvcTN3cWU2ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LpQuxhwDhzLCEKVYFh/giphy.gif"/>
+</body>"""
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
