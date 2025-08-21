a1c4e144bbac6b8fe340bd679a0d639fb3854f63
Bethany <bethany.schoen@bupa.com>
2025-08-21 09:54:18 +0000

    month and day not callable

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def is_birthday(birthday):
   tg = TextGenerationGCP()
   today = datetime.today()
 
-  if (today.month() == birthday_datetime.month()) & (today.day() == birthday_datetime.day()):
+  if (today.month == birthday_datetime.month) & (today.day == birthday_datetime.day):
     birthday_prompt = p.birthday
     birthday_haiku = tg.generate_text(contents=birthday_prompt)
 
